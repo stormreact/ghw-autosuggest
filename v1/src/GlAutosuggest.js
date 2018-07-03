1a978e3fbb7e21a5fba0647acbed1d221788615b
Michael Angerman <stormasm@gmail.com>
2018-07-03 09:15:12 -0700

    still one bug left

--- v1/src/GlAutosuggest.js
+++ v1/src/GlAutosuggest.js
@@ -3,19 +3,24 @@ import PropTypes from "prop-types";
 import Autosuggest from "react-autosuggest";
 import match from "autosuggest-highlight/match";
 import parse from "autosuggest-highlight/parse";
-import TextField from "material-ui/TextField";
-import Paper from "material-ui/Paper";
-import { MenuItem } from "material-ui/Menu";
-import Table, {
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow
-} from "material-ui/Table";
-import { withStyles } from "material-ui/styles";
 
-import GridList, { GridListTile } from "material-ui/GridList";
+import { withStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+
+import { MenuItem } from "@material-ui/core/MenuItem";
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import GhCard from "./GhCard";
+
 //import lightGreen from "material-ui/colors/lightGreen";
 
 /*
